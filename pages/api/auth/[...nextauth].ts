53ace30792c309ce399fa63450406444e6ae9855
himanshu-plabs <h@plotlabs.com>
2025-01-06 20:13:08 +0530

    Update authentication routes to redirect to the registration page
    - Changed signIn and error page routes from "/login" to "/register" in the NextAuthOptions configuration.
    - This update aligns the authentication flow with the new user registration process implemented in previous commits.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -112,8 +112,8 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   pages: {
-    signIn: "/login",
-    error: "/login",
+    signIn: "/register",
+    error: "/register",
     verifyRequest: "/verify-request",
   },
   session: {
